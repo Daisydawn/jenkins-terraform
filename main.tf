f6361b6c245f1304b2426bad63f2b0fdd916c25f
Daisydawn <134785310+Daisydawn@users.noreply.github.com>
2023-06-03 21:04:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "aws_instance" "my_vm" {
    Name = var.name_tag,
  }
 }
+resource "aws_s3_bucket" "jenkinss3" {
+  bucket = "my-jenkinss3-bucket"
 
+  tags = {
+    Name        = "jenkinss3"
